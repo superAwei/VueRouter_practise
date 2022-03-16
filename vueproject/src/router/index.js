7c0f23362cb3c69d8b6744e39cf01e5c6ffa5238
ＡＷＥＩ <awei@AWEIde-MacBook-Pro.local>
2022-03-16 23:46:19 +0800

    第一次上傳檔案到github

---
+++ vueproject/src/router/index.js
@@ -0,0 +1,13 @@
+//官方文件
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+//自訂要有的分頁
+import Home from '@/components/HelloWorld';
+
+// 要使用 VueRouter 就要打這一段
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    
+});
